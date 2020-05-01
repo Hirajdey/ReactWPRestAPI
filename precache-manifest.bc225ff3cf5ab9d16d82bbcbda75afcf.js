90ddb1fa485f69b0cb65b7c9ba6c31ea0ba4c5b8
RakeshChaturya <rakeshchaturya@gmail.com>
2020-05-01 20:44:13 +0530

    Updates

---
+++ precache-manifest.bc225ff3cf5ab9d16d82bbcbda75afcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d1aedd8d9c2ad8b234ee4476f13d65",
+    "url": "/react-with-wordpress/index.html"
+  },
+  {
+    "revision": "68e0f11ac44c3a5f6c63",
+    "url": "/react-with-wordpress/static/css/main.d2532636.chunk.css"
+  },
+  {
+    "revision": "7a0a20a63026a7d63bfd",
+    "url": "/react-with-wordpress/static/js/2.2291d7ea.chunk.js"
+  },
+  {
+    "revision": "ea4216b0c40fe4334a0788bd66f0a92b",
+    "url": "/react-with-wordpress/static/js/2.2291d7ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68e0f11ac44c3a5f6c63",
+    "url": "/react-with-wordpress/static/js/main.81b37046.chunk.js"
+  },
+  {
+    "revision": "e66513c01f34fea78c20",
+    "url": "/react-with-wordpress/static/js/runtime-main.852b0ca0.js"
+  },
+  {
+    "revision": "70486fce1414047536eed4151e84b48f",
+    "url": "/react-with-wordpress/static/media/loding.70486fce.gif"
+  }
+]);
